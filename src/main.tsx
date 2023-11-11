cf34fa1256853f9903f60bf639a08fc3e4689298
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-10 22:13:55 -0300

    A great beginning ✨

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "~/components/root/App";
+import { StrictMode } from "react";
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode!);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
