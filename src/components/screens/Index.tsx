567c15c6dffb10ab739425981caa884e4e01dec6
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-11 20:38:30 -0300

    Removing condition

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,12 +1,9 @@
-import { useAuthState } from '~/components/contexts/UserContext';
 import { SignInWithGoogle } from '~/components/domain/auth/SignInWithGoogle';
-import { SignOutButton } from '~/components/domain/auth/SignOutButton';
 import { Head } from '~/components/shared/Head';
 import { SignInWithMicrosoft } from '~/components/domain/auth/SignInWithMicrosoft';
 import { SignInWithGitHub } from '~/components/domain/auth/SignInWithGithub';
 
 function Index() {
-  const { state } = useAuthState();
   return (
     <>
       <Head title="TOP PAGE" />
@@ -14,11 +11,10 @@ function Index() {
         <div className="text-center hero-content">
           <div>
             <div className="mt-4 grid gap-2">
-              {state.state === 'UNKNOWN' ? null : state.state === 'SIGNED_OUT' ? <>
+              
                 <SignInWithGoogle />
                 <SignInWithMicrosoft />
                 <SignInWithGitHub />
-              </> : <SignOutButton />}
             </div>
           </div>
         </div>
