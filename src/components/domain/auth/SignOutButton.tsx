ed91c40b224325670c96ef83eca114fac194bf63
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-11 21:35:57 -0300

    improving sign out fn

--- src/components/domain/auth/SignOutButton.tsx
+++ src/components/domain/auth/SignOutButton.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '~/lib/firebase';
 
 export const SignOutButton = () => {
   const navigate = useNavigate();
-  const auth = useAuth();
-  const signOut = () => auth.signOut().then(() => { 
-    navigate('/');
-  });
+  const auth = useAuth;
 
+  async function handleClick() {
+    await auth().signOut();
+    navigate('/');
+  };
+  
   return (
-    <a onClick={signOut} className="btn btn-outline btn-error btn-sm">Sign out</a>
+    <a onClick={handleClick}  className="btn btn-outline btn-error btn-sm">Sign out</a>
   )
 };
