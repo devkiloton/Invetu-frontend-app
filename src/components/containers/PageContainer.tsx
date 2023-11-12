08a79db80fd0394a6c9043199cd6493a2e0a9900
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 19:50:15 -0300

    Improving page container

--- src/components/containers/PageContainer.tsx
+++ src/components/containers/PageContainer.tsx
@@ -1,3 +1,3 @@
 export default function PageContainer({ children }: any) {
-  return <div className="flex flex-col mx-8 my-2 rounded-2xl bg-base-300">{children}</div>;
+  return <div className="flex flex-col mx-8 my-2 pt-2 gap-4 rounded-2xl">{children}</div>;
 }
