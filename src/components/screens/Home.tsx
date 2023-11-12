29192e75d7ff7d63ed565eabdf45c3ff671a73e3
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 20:21:27 -0300

    Redesigning home

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import PageContainer from '../containers/PageContainer';
 import AddStocksForm from '../forms/AddStocksForm';
 import AccountStats from '../shared/AccountStats';
+import InvestmentCard from '../shared/InvestmentCard';
+import { firebaseClient } from '~/clients/firebase-client/firebase-client';
+import { useAuth } from '~/lib/firebase';
+import { Stock } from '~/clients/firebase-client/models/Investments';
 
 export default function Home() {
+  const [investments, setInvestments] = useState<Array<Stock>>([]);
+  const auth = useAuth();
+  useEffect(() => { 
+    if (auth.currentUser?.uid !== undefined) {
+      firebaseClient().firestore.investments.stocks.get(auth.currentUser.uid).then((investiments) => { 
+        setInvestments(investiments.stocks)
+      })
+    }
+  }, [])
   return (
     <PageContainer>
       <AccountStats />
-      <AddStocksForm />
+      <div className='flex gap-x-4'>
+        <AddStocksForm />
+        <div className='w-full flex flex-col gap-4'>
+        {
+          investments.map((investment) => { 
+            return <InvestmentCard {...investment} />
+          })
+        }
+        </div>
+      </div>
     </PageContainer>
   );
 }
