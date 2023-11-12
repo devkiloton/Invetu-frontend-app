c76db43504659ff1af7eab60a6b9d79e5673f22d
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 19:49:33 -0300

    Updating firebase client

--- src/clients/firebase-client/firebase-client.ts
+++ src/clients/firebase-client/firebase-client.ts
@@ -1,31 +1,35 @@
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { arrayUnion, doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useFirestore } from '~/lib/firebase';
-
-type Stock = {
-  ticker: string;
-  price: number;
-  amount: number;
-  date: number;
-  currency: string;
-  userID: string;
-};
+import { Investments, Stock } from './models/Investments';
 
 export const firebaseClient = () => {
   const firestore = useFirestore();
   const client = {
     firestore: {
-      stocks: {
-        add: async (data: Stock) => {
-          await updateDoc(doc(firestore, 'investments', `${data.userID}`), {
-            stocks: arrayUnion({
-              ticker: data.ticker,
-              price: data.price,
-              amount: data.amount,
-              date: data.date,
-              currency: data.currency,
-              userID: data.userID,
-            }),
-          });
+      investments: {
+        get: async (userID: string): Promise<Investments> => {
+          const docRef = doc(firestore, 'investments', `${userID}`);
+          const docSnap = await getDoc(docRef);
+          return docSnap.data() as Investments;
+        },
+        stocks: {
+          add: async (data: Stock): Promise<void> => {
+            await updateDoc(doc(firestore, 'investments', `${data.userID}`), {
+              stocks: arrayUnion({
+                ticker: data.ticker,
+                price: data.price,
+                amount: data.amount,
+                date: data.startDate,
+                currency: data.currency,
+                userID: data.userID,
+              }),
+            });
+          },
+          get: async (userID: string): Promise<Investments> => {
+            const docRef = doc(firestore, 'investments', `${userID}`);
+            const docSnap = await getDoc(docRef);
+            return docSnap.data() as Investments;
+          },
         },
       },
     },
