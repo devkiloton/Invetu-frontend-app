32554158182d505e3507273c3e841c144694d4dc
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 11:11:11 -0300

    Creating a container

---
+++ src/components/containers/PageContainer.tsx
@@ -0,0 +1,7 @@
+export default function PageContainer({ children}: any) {
+  return (
+      <div className="flex flex-col mx-5 my-2 rounded-2xl bg-base-300">
+       {children}
+      </div>
+  )
+}
