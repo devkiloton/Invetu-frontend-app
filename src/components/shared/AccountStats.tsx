4ebc9584c61457c49acab589b1cd47934987145b
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 20:20:58 -0300

    Removing account stats actions

--- src/components/shared/AccountStats.tsx
+++ src/components/shared/AccountStats.tsx
@@ -1,22 +1,15 @@
 export default function AccountStats() {
   return (
-    <div className="w-full p-4">
-      <div className="stats bg-primary text-primary-content w-full">
+    <div className="w-full">
+      <div className="stats bg-primary text-primary-content w-full bordered">
         <div className="stat">
           <div className="stat-title">Account balance</div>
           <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm btn-success">Add funds</button>
-          </div>
         </div>
 
         <div className="stat">
           <div className="stat-title">Current balance</div>
           <div className="stat-value">$89,400</div>
-          <div className="stat-actions gap-x-2 flex">
-            <button className="btn btn-sm">Withdrawal</button>
-            <button className="btn btn-sm">deposit</button>
-          </div>
         </div>
         <div className="stat">
           <div className="stat-title">Account balance</div>
