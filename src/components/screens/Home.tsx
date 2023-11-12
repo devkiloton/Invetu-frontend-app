b3ae3380f73417bb2a87ec8e7d92e1febca66a56
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 11:14:13 -0300

    Updating home

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,3 +1,12 @@
+import PageContainer from "../containers/PageContainer";
+import AddStocksForm from "../forms/AddStocksForm";
+import AccountStats from "../shared/AccountStats";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <PageContainer>
+      <AccountStats />
+      <AddStocksForm />
+    </PageContainer>
+  );
 }
