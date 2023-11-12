d9bbf76d9c25c706db244e42f8cfacbf6be6676f
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-11 21:38:14 -0300

    Making useffect reactive

--- src/components/router/handlers/AuthHandler.tsx
+++ src/components/router/handlers/AuthHandler.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthState } from '~/components/contexts/UserContext';
 
 export default function AuthHandler() {
     const { state } = useAuthState();
     const navigate = useNavigate();
-  return state.state === 'SIGNED_IN' ? navigate('/home') : navigate('/');
+    
+    useEffect(() => { 
+        if (state.state !== 'UNKNOWN') {
+            state.state === 'SIGNED_IN' ? navigate('/home') : navigate('/')
+        }
+    }, [state.state]);
 }
