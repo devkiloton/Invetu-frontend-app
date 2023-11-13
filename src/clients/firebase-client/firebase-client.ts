07fe79273f619e8d715c0513d33c1bb08b7d04fa
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 01:56:10 -0300

    Making firebase update invested amount

--- src/clients/firebase-client/firebase-client.ts
+++ src/clients/firebase-client/firebase-client.ts
@@ -14,7 +14,30 @@ export const firebaseClient = () => {
         },
         stocks: {
           add: async (data: Stock): Promise<void> => {
+            const investments = await client.firestore.investments.get(data.userID);
+            const stock = investments.stocks.find(stock => stock.ticker === data.ticker);
+            // If stock already exists, update it summing the amount and dividing the price by the new amount
+            if (stock) {
+              const newAmount = stock.amount + data.amount;
+              const newPrice = (stock.price * stock.amount + data.price * data.amount) / newAmount;
+              const newStocks = investments.stocks.map(stock => {
+                if (stock.ticker === data.ticker) {
+                  return {
+                    ...stock,
+                    amount: newAmount,
+                    price: newPrice,
+                  };
+                }
+                return stock;
+              });
+              await updateDoc(doc(firestore, 'investments', `${data.userID}`), {
+                investedAmount: investments.investedAmount + data.price * data.amount,
+                stocks: newStocks,
+              });
+              return;
+            }
             await updateDoc(doc(firestore, 'investments', `${data.userID}`), {
+              investedAmount: investments.investedAmount + data.price * data.amount,
               stocks: arrayUnion({
                 ticker: data.ticker,
                 price: data.price,
