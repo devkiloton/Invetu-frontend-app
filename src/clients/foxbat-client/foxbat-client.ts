dd14f0ca1864c57b7887ff16b96e93fb1e1b63f1
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 03:38:23 -0300

    Improving var access

--- src/clients/foxbat-client/foxbat-client.ts
+++ src/clients/foxbat-client/foxbat-client.ts
@@ -2,21 +2,20 @@ import { FuzzyAPI } from './models/FuzzyAPI';
 import { RawStocksAPI, StocksAPI } from './models/StocksAPI';
 
 export const foxbatClient = () => {
+  const API_URL = import.meta.env.VITE_API_ORIGIN;
   const client = {
     stocks: {
       fuzzy: async (ticker: string): Promise<Array<string>> => {
-        const stocks = await fetch(`${import.meta.env.API_URL}/stocks/fuzzy/${ticker}`);
+        const stocks = await fetch(`${API_URL}/stocks/fuzzy/${ticker}`);
         const stocksJson = (await stocks.json()) as FuzzyAPI;
         return stocksJson.stocks;
       },
       tickerInfo: async (ticker: string) => {
-        return fetch(`${import.meta.env.API_URL}/${ticker}`).then(res => res.json());
+        return fetch(`${API_URL}/${ticker}`).then(res => res.json());
       },
       findMany: async (tickers: string[]): Promise<StocksAPI> => {
         const tickersString = tickers.join(',');
-        const stocks: RawStocksAPI = await fetch(`${import.meta.env.API_URL}/stocks/${tickersString}`).then(res =>
-          res.json(),
-        );
+        const stocks: RawStocksAPI = await fetch(`${API_URL}/stocks/${tickersString}`).then(res => res.json());
         return stocks[0];
       },
     },
