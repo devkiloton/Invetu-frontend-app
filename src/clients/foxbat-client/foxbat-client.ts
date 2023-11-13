bfceea82d59e8501f03a8800f00a82fa77e7fcc9
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 03:03:03 -0300

    Accessing var in other way

--- src/clients/foxbat-client/foxbat-client.ts
+++ src/clients/foxbat-client/foxbat-client.ts
@@ -5,16 +5,16 @@ export const foxbatClient = () => {
   const client = {
     stocks: {
       fuzzy: async (ticker: string): Promise<Array<string>> => {
-        const stocks = await fetch(`${process.env.API_URL}/stocks/fuzzy/${ticker}`);
+        const stocks = await fetch(`${import.meta.env.API_URL}/stocks/fuzzy/${ticker}`);
         const stocksJson = (await stocks.json()) as FuzzyAPI;
         return stocksJson.stocks;
       },
       tickerInfo: async (ticker: string) => {
-        return fetch(`${process.env.API_URL}/${ticker}`).then(res => res.json());
+        return fetch(`${import.meta.env.API_URL}/${ticker}`).then(res => res.json());
       },
       findMany: async (tickers: string[]): Promise<StocksAPI> => {
         const tickersString = tickers.join(',');
-        const stocks: RawStocksAPI = await fetch(`${process.env.API_URL}/stocks/${tickersString}`).then(res =>
+        const stocks: RawStocksAPI = await fetch(`${import.meta.env.API_URL}/stocks/${tickersString}`).then(res =>
           res.json(),
         );
         return stocks[0];
