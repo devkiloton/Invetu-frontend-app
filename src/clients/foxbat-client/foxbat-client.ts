2429aadf7ae2d2f552b3a3e7efd76befc949d3f9
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 01:59:17 -0300

    Creating client api

---
+++ src/clients/foxbat-client/foxbat-client.ts
@@ -0,0 +1,26 @@
+import { FuzzyAPI } from './models/FuzzyAPI';
+import { StockAPI } from './models/StockAPI';
+import { RawStocksAPI, StocksAPI } from './models/StocksAPI';
+
+export const foxbatClient = () => {
+  const client = {
+    stocks: {
+      fuzzy: async (ticker: string): Promise<Array<string>> => {
+        const stocks = await fetch(`${API}/stocks/fuzzy/${ticker}`);
+        const stocksJson = (await stocks.json()) as FuzzyAPI;
+        return stocksJson.stocks;
+      },
+      tickerInfo: async (ticker: string) => {
+        return fetch(`${process.env.API_URL}/${ticker}`).then(res => res.json());
+      },
+      findMany: async (tickers: string[]): Promise<StocksAPI> => {
+        const tickersString = tickers.join(',');
+        const stocks: RawStocksAPI = await fetch(`${process.env.API_URL}/stocks/${tickersString}`).then(res =>
+          res.json(),
+        );
+        return stocks[0];
+      },
+    },
+  };
+  return client;
+};
