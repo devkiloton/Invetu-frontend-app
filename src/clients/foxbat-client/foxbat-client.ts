16db113073b3cba1966e9a42abe3f5a3f45997c1
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-16 04:54:24 -0300

    Adding find history to the foxbat client

--- src/clients/foxbat-client/foxbat-client.ts
+++ src/clients/foxbat-client/foxbat-client.ts
@@ -1,5 +1,7 @@
 import { FuzzyAPI } from './models/FuzzyAPI';
+import { HistoryAPI } from './models/HistoryAPI';
 import { RawStocksAPI, StocksAPI } from './models/StocksAPI';
+import { FindHistoryParams } from './types/find-sitory-params';
 
 export const foxbatClient = () => {
   const API_URL = import.meta.env.VITE_API_ORIGIN;
@@ -15,6 +17,12 @@ export const foxbatClient = () => {
           res.json(),
         );
         return stock[0];
+      },
+      findHistory: async (info: FindHistoryParams): Promise<HistoryAPI> => {
+        const data = await fetch(
+          `${API_URL}/stocks/history/${info.ticker}?range=${info.range}&interval=${info.interval}`,
+        ).then(res => res.json());
+        return data[0];
       },
       findMany: async (tickers: string[]): Promise<StocksAPI> => {
         const tickersString = tickers.join(',');
