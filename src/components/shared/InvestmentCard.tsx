dbff3ba43af7f3b516a7eb943d59c967f6ef9373
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-16 05:00:01 -0300

    Querying data(improve that later)

--- src/components/shared/InvestmentCard.tsx
+++ src/components/shared/InvestmentCard.tsx
@@ -11,7 +11,9 @@ export default function InvestmentCard(
   props: Stock & { investedAmount: number; currentBalance: number },
 ) {
   const [stockInfo, setStockInfo] = useState<StockAPI | null>(null);
+  const [chartData, setChartData] = useState<{ dates: string[], prices: number[] } | null>(null)
   const auth = useAuth();
+
   useEffect(() => {
     foxbatClient()
       .stocks.tickerInfo(props.ticker)
@@ -19,6 +21,26 @@ export default function InvestmentCard(
         setStockInfo(response);
       });
   }, []);
+
+  useEffect(() => {
+    const range = getNearestDateRange(new Date(props.startDate).toISOString());
+    foxbatClient().stocks.findHistory({
+      ticker: [props.ticker],
+      range,
+      interval: getBestInterval(range),
+    }).then(response => {
+
+      const dates = response.results[0].historicalDataPrice
+        // removing 10800000 ms (3 hours) to adjust to the brazilian timezone
+        .map(price => (price.date * 1000) - 10800000)
+        .filter(value => value > Date.parse(props.startDate))
+        .map(value => new Date(value).toISOString())
+      setChartData({
+        dates,
+        prices: response.results[0].historicalDataPrice.slice(dates.length * -1).map(price => price.close)
+      })
+    })
+   }, [stockInfo])
 
   function deleteStock() {
     if (auth.currentUser?.uid !== undefined)
@@ -80,7 +102,7 @@ export default function InvestmentCard(
             </details>
           </div>
           {!isNull(stockInfo) && (
-            <div className="flex flex-col gap-x-2">
+            <div className="flex flex-col min-[768px]:flex-row gap-x-2">
               <span className="text-sm  font-semibold">
                 <span className="text-xs font-normal">Amount:</span>{' '}
                 {props.amount}
@@ -113,8 +135,9 @@ export default function InvestmentCard(
               </span>
             </div>
 
-)}
-<InvestementCardChart />
+          )}
+          {!isNull(chartData) && (<InvestementCardChart dates={chartData.dates} prices={chartData.prices} />)}
+
           <div className="card-actions">
             <button disabled className="btn btn-primary w-full">
               Mais detalhes
@@ -124,4 +147,60 @@ export default function InvestmentCard(
       </div>
     </>
   );
+}
+
+
+function getNearestDateRange(startDate: string): "1d" | "5d" | "1mo" | "3mo" | "6mo" | "1y" | "2y" | "5y" | "10y" | "ytd" | "max" {
+  const today = new Date();
+  const start = new Date(startDate);
+  const diffTime = Math.abs(today.getTime() - start.getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  if (diffDays <= 1) return "1d";
+  else if (diffDays <= 5) return "5d";
+  else if (diffDays <= 30) return "1mo";
+  else if (diffDays <= 90) return "3mo";
+  else if (diffDays <= 180) return "6mo";
+  else if (diffDays <= 365) return "1y";
+  else if (diffDays <= 730) return "2y";
+  else if (diffDays <= 1825) return "5y";
+  else if (diffDays <= 3650) return "10y";
+  else return "max";
+}
+
+function getBestInterval(range: "1d" | "5d" | "1mo" | "3mo" | "6mo" | "1y" | "2y" | "5y" | "10y" | "ytd" | "max"):   '1m'
+| '2m'
+| '5m'
+| '15m'
+| '30m'
+| '60m'
+| '90m'
+| '1h'
+| '1d'
+| '5d'
+| '1wk'
+| '1mo'
+| '3mo' {
+
+  switch (range) { 
+    case "1d":
+      return "5m";
+    case "5d":
+      return "1h"
+    case "1mo":
+      return "1d"
+    case "3mo":
+      return "1d"
+    case "6mo":
+      return "1d"
+    case "1y":
+      return "5d"
+    case "2y":
+      return "1wk"
+    case "5y":
+      return "1mo"
+    case "10y":
+      return "3mo"
+    case "ytd":
+      return "1d"
+  }
 }
