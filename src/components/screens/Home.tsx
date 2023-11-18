b6c55f38ac52d53b8a381d7633f97a090e9668c7
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-18 12:46:09 -0300

    Implementing radial chart in the home page

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -10,6 +10,7 @@ import { joinStockData } from '~/helpers/join-stock-data';
 import { Dialog } from '@headlessui/react';
 import { foxbatClient } from '~/clients/foxbat-client/foxbat-client';
 import { Head } from '../shared/Head';
+import RadialChart from '../shared/RadialChart';
 
 export default function Home() {
   const [investments, setInvestments] = useState<Array<Stock>>([]);
@@ -52,58 +53,66 @@ export default function Home() {
   }, []);
   return (
     <>
-    <Head title="Foxbat | Home" />
-    <PageContainer>
-      <AccountStats
-        investedAmount={investedAmount}
-        currentBalance={currentBalance}
-      />
-      <div className="flex gap-x-4">
-        <div className="w-full h-full sticky top-24 max-w-120 hidden min-[768px]:block ">
-          <AddStocksForm />
+      <Head title="Foxbat | Home" />
+      <PageContainer>
+        <AccountStats
+          investedAmount={investedAmount}
+          currentBalance={currentBalance}
+        />
+        <div>
+          <div className="w-full"></div>
+          <div className="glassy-border rounded-2xl w-fit p-8">
+
+          <h1 className="font-semibold">Resultados desse mês</h1>
+        <RadialChart />
+          </div>
         </div>
-        <div className="w-full flex flex-col gap-4">
-          {investments.map(investment => {
-            return (
-              <InvestmentCard
-                key={crypto.randomUUID()}
-                {...investment}
-                currentBalance={currentBalance}
-                investedAmount={investedAmount}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="btn btn-primary btn-circle fixed bottom-5 right-5 ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 rotate-45"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-[100]">
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <Dialog.Panel className="max-w-120 w-full overflow-scroll max-h-[90vh]">
+        <div className="flex gap-x-4">
+          <div className="w-full h-full sticky top-24 max-w-120 hidden min-[768px]:block ">
             <AddStocksForm />
-          </Dialog.Panel>
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            {investments.map(investment => {
+              return (
+                <InvestmentCard
+                  key={crypto.randomUUID()}
+                  {...investment}
+                  currentBalance={currentBalance}
+                  investedAmount={investedAmount}
+                />
+              );
+            })}
+          </div>
         </div>
-      </Dialog>
-    </PageContainer>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="btn btn-primary btn-circle fixed bottom-5 right-5 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 rotate-45"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative z-[100]">
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+          <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+            <Dialog.Panel className="max-w-120 w-full overflow-scroll max-h-[90vh]">
+              <AddStocksForm />
+            </Dialog.Panel>
+          </div>
+        </Dialog>
+      </PageContainer>
     </>
   );
 }
