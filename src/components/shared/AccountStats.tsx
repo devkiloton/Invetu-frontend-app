a30d7932528a49809b00460c1b9bbc3c654d5604
devkiloton <devkiloton@gmail.com>
2023-12-03 13:41:52 -0300

    Account stats responsive

--- src/components/shared/AccountStats.tsx
+++ src/components/shared/AccountStats.tsx
@@ -4,7 +4,7 @@ export default function AccountStats(props: {
 }) {
   return (
     <div className="w-full">
-      <div className="stats bg-primary text-primary-content w-full bordered">
+      <div className="stats bg-primary text-primary-content w-full bordered flex flex-col md:flex-row">
         <div className="stat border-base-100 border-opacity-20">
           <div className="stat-title text-neutral">Total investido</div>
           <div className="stat-value">R$ {props.investedAmount.toFixed(2)}</div>
