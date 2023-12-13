967e5f551a0113707452f2fe3547d539f3721f4c
devkiloton <devkiloton@gmail.com>
2023-12-13 00:19:12 -0300

    Improving login page

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -2,24 +2,48 @@ import { SignInWithGoogle } from '~/components/domain/auth/SignInWithGoogle';
 import { Head } from '~/components/shared/Head';
 import { SignInWithMicrosoft } from '~/components/domain/auth/SignInWithMicrosoft';
 import { SignInWithGitHub } from '~/components/domain/auth/SignInWithGithub';
+import CoupleTouchingSaturn from '~/assets/illustrations/couple-touching-saturn.svg';
 
-function Index() {
+const Index = () => {
   return (
     <>
       <Head title="Login" />
       <div className="hero" style={{ height: 'calc(100vh - 80px)' }}>
-        <div className="text-center hero-content">
+        <div className="text-center h-full w-full hero-content justify-evenly flex-col-reverse md:flex-row">
+          <div className="mt-4 grid gap-2">
+            <SignInWithGoogle />
+            <SignInWithMicrosoft />
+            <SignInWithGitHub />
+            <p className="text-xs text-center mt-8">
+              Ao criar uma conta, você concorda com
+              <br /> nossos{' '}
+              <a className="text-blue-600" href="/#">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a className="text-blue-600" href="/#">
+                privacidade
+              </a>
+              .
+            </p>
+          </div>
+          <div className="divider divider-horizontal" />
+
           <div>
-            <div className="mt-4 grid gap-2">
-              <SignInWithGoogle />
-              <SignInWithMicrosoft />
-              <SignInWithGitHub />
-            </div>
+            <h1 className="text-4xl font-bold leading-9 text-start px-4">
+              Alcance o impossível <br /> com a{' '}
+              <span className="text-primary">Invetu</span>
+            </h1>
+            <img
+              className="max-w-120"
+              src={CoupleTouchingSaturn}
+              alt="Casal tocando saturno"
+            />
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Index;
