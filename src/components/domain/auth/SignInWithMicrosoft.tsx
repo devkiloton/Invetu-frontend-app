928320c299520256bad74ade1115fa9db1f6e05a
devkiloton <devkiloton@gmail.com>
2023-12-12 22:38:58 -0300

    New sign in button design

--- src/components/domain/auth/SignInWithMicrosoft.tsx
+++ src/components/domain/auth/SignInWithMicrosoft.tsx
@@ -2,6 +2,7 @@ import { OAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '~/lib/firebase';
 
+// #TODO: It's not DRY. It should be refactored.
 export const SignInWithMicrosoft = () => {
   const navigate = useNavigate();
 
@@ -18,7 +19,17 @@ export const SignInWithMicrosoft = () => {
     <button
       onClick={handleClick}
       type="button"
-      className="btn btn-primary normal-case min-w-60">
+      className="btn btn-active btn-ghost normal-case gap-3 justify-start px-6">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+        width="24"
+        height="24">
+        <path d="M0 0h15.206v15.206H0z" fill="#f25022" />
+        <path d="M16.794 0H32v15.206H16.794z" fill="#7fba00" />
+        <path d="M0 16.794h15.206V32H0z" fill="#00a4ef" />
+        <path d="M16.794 16.794H32V32H16.794z" fill="#ffb900" />
+      </svg>
       Entrar com Microsoft
     </button>
   );
