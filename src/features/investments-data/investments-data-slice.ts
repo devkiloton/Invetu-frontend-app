b5fb451e0b28d423ccf53c21422993dacecff5b2
devkiloton <devkiloton@gmail.com>
2023-12-21 00:32:52 -0300

    Handling max range

--- src/features/investments-data/investments-data-slice.ts
+++ src/features/investments-data/investments-data-slice.ts
@@ -6,6 +6,7 @@ import {
   Result,
 } from '~/clients/firebase-client/models/history-stock-br';
 import { useAuth } from '~/lib/firebase';
+import getStocksHighestDateRange from '~/helpers/get-stocks-highest-date-range';
 type InvestmentsData = {
   data: Record<string, Result>;
   asyncState: AsyncStateRedux;
@@ -30,19 +31,63 @@ export const fetchAllInvestmentsData: any = createAsyncThunk(
     if (!uid) throw new Error('User not found');
     const investments = await firebaseClient().firestore.investments.get(uid);
     const joinedStocks = investments.stocks.map(stock => stock.ticker);
-    return await firebaseClient()
-      .functions.findHistoryStocksBR(joinedStocks, 'max', '1d')
-      .then(res => {
-        res[0] as HistoryStockBR;
-        const data = res[0].results.reduce(
-          (acc, result) => {
-            acc[result.symbol] = result;
-            return acc;
-          },
-          {} as Record<string, Result>,
+
+    const stocksData = getStocksHighestDateRange(investments.stocks);
+
+    if (stocksData.stocksMaxRangeTickers.length === 0) {
+      return await firebaseClient()
+        .functions.findHistoryStocksBR(
+          joinedStocks,
+          stocksData.highestRange,
+          '1d',
+        )
+        .then(res => {
+          res[0] as HistoryStockBR;
+          const data = res[0].results.reduce(
+            (acc, result) => {
+              acc[result.symbol] = result;
+              return acc;
+            },
+            {} as Record<string, Result>,
+          );
+          return { data };
+        });
+    } else {
+      // Create two promises, one for the stocks with max range and another for the stocks with the highest range, then join them in a way that has the same signature as the first if block
+      const maxRangePromise =
+        stocksData.stocksMaxRangeTickers.length === 0
+          ? firebaseClient().functions.findHistoryStocksBR(
+              stocksData.stocksMaxRangeTickers,
+              'max',
+              '5d',
+            )
+          : new Promise<[]>(() => []);
+      const highestRangePromise =
+        firebaseClient().functions.findHistoryStocksBR(
+          joinedStocks,
+          stocksData.highestRange,
+          '1d',
         );
-        return { data };
-      });
+      const [maxRange, highestRange] = await Promise.all([
+        maxRangePromise,
+        highestRangePromise,
+      ]);
+      const maxRangeData = maxRange[0].results.reduce(
+        (acc, result) => {
+          acc[result.symbol] = result;
+          return acc;
+        },
+        {} as Record<string, Result>,
+      );
+      const highestRangeData = highestRange[0].results.reduce(
+        (acc, result) => {
+          acc[result.symbol] = result;
+          return acc;
+        },
+        {} as Record<string, Result>,
+      );
+      return { data: { ...maxRangeData, ...highestRangeData } };
+    }
   },
 );
 
@@ -55,6 +100,9 @@ export const investmentsDataSlice = createSlice({
         ...state.data,
         [action.payload.symbol]: action.payload,
       };
+    },
+    deleteStockData: (state, action: PayloadAction<string>) => {
+      delete state.data[action.payload];
     },
   },
   extraReducers: builder => {
@@ -78,5 +126,5 @@ export const investmentsDataSlice = createSlice({
   },
 });
 
-export const { addStockData } = investmentsDataSlice.actions;
+export const { addStockData, deleteStockData } = investmentsDataSlice.actions;
 export const investmentsDataReducer = investmentsDataSlice.reducer;
