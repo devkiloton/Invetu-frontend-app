d2af0794326a48cbd070f34eb64ccf3db43e968d
devkiloton <devkiloton@gmail.com>
2023-12-22 19:03:32 -0300

    Adding analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,6 +8,7 @@ import {
 } from 'firebase/auth';
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { connectStorageEmulator, getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 let firebaseApp: FirebaseApp;
 const useEmulator = () => import.meta.env.VITE_USE_FIREBASE_EMULATOR;
@@ -23,6 +24,7 @@ export const setupFirebase = () => {
       messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
       appId: import.meta.env.VITE_FIREBASE_APPID,
     });
+    getAnalytics(firebaseApp);
   } catch (error) {
     console.error({ error });
   }
