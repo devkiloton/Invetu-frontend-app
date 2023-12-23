14c77383759dbe15cdf31422abf8bf8678aeebf0
devkiloton <devkiloton@gmail.com>
2023-12-23 16:47:09 -0300

    Adding new functions to index

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,6 @@ exports.createUserEntities =
 exports.findHistoryStocksBR =
   require('./find-history-stocks-br').findHistoryStocksBR;
 exports.fuzzyStocksBR = require('./fuzzy-stocks-br').fuzzyStocksBR;
+exports.findAllCryptos = require('./find-all-cryptos').findAllCryptos;
+exports.findCryptosData = require('./find-cryptos-data').findCryptosData;
+exports.findFiats = require('./find-fiats').findFiats;
