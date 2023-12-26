d3b2f4ec9b74d696caf3ddb81a562d63a9e475d4
devkiloton <devkiloton@gmail.com>
2023-12-26 00:01:05 -0300

    Implementing 2 new enpoints from bacen

--- src/clients/bacen-client/index.ts
+++ src/clients/bacen-client/index.ts
@@ -1,4 +1,6 @@
 import { CDI } from './models/cdi';
+import { HistoryCDI } from './models/history-cdi';
+import { HistoryIPCA } from './models/history-ipca';
 
 export const bacenClient = () => {
   const API_URL = import.meta.env.VITE_API_BACEN;
@@ -10,6 +12,20 @@ export const bacenClient = () => {
           `${API_URL}/dados/serie/bcdata.sgs.4391/dados/ultimos/1?formato=json`,
         ).then(res => res.json());
         return cdi;
+      },
+      findHistory: async (): Promise<HistoryCDI> => {
+        const cdiHistory = (await fetch(
+          `${API_URL}/dados/serie/bcdata.sgs.4391/dados?formato=json`,
+        ).then(res => res.json())) as HistoryCDI;
+        return cdiHistory;
+      },
+    },
+    ipca: {
+      findHistory: async (): Promise<HistoryIPCA> => {
+        const ipcaHistory = (await fetch(
+          `${API_URL}/dados/serie/bcdata.sgs.433/dados?formato=json`,
+        ).then(res => res.json())) as HistoryIPCA;
+        return ipcaHistory;
       },
     },
   };
