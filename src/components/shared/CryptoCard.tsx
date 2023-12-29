cb73ff3b37278a5b7b8de4860cca5a95ab4f63bd
devkiloton <devkiloton@gmail.com>
2023-12-29 19:58:49 -0300

    Updating crypto card

--- src/components/shared/CryptoCard.tsx
+++ src/components/shared/CryptoCard.tsx
@@ -8,6 +8,9 @@ import { useCustomSelector } from '~/hooks/use-custom-selector';
 import { CryptoCurrency } from '~/clients/firebase-client/models/status-cryptos';
 import { HistoryCryptoUS } from '~/clients/firebase-client/models/data-cryptos';
 import useDeleteCrypto from '~/hooks/use-delete-crypto';
+import getProfit from '~/helpers/get-profit';
+import getStockAllocation from '~/helpers/get-stock-allocation';
+import getBalance from '~/helpers/get-balance';
 
 function CryptoCard(
   props: Crypto & { investedAmount: number; currentBalance: number },
@@ -78,91 +81,86 @@ function CryptoCard(
   }, [cryptoInfo]);
 
   return (
-    <>
-      <div className="card bg-base-100 shadow-xl glassy-border z-0">
-        <div className="card-body p-4 md:p-8">
-          <div className="flex justify-between">
-            <div className="flex items-center gap-x-2">
-              <img className="h-8 w-8 rounded" src={cryptoInfo?.status.icon} />
+    <div className="card bg-base-100 shadow-xl glassy-border z-0">
+      <div className="card-body p-4 md:p-8">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-x-2">
+            <img className="h-8 w-8 rounded" src={cryptoInfo?.status.icon} />
 
-              <h2 className="card-title">{cryptoInfo?.status.name}</h2>
-            </div>
-
-            <details className="dropdown dropdown-end">
-              <summary className="m-1 btn btn-ghost btn-circle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 20 20">
-                  <circle cx="10" cy="4" r="2" fill="currentColor" />
-                  <circle cx="10" cy="10" r="2" fill="currentColor" />
-                  <circle cx="10" cy="16" r="2" fill="currentColor" />
-                </svg>
-              </summary>
-              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 glassy-border">
-                <li>
-                  <button onClick={deleteSelectedCrypto}>Deletar</button>
-                </li>
-                <li className="disabled">
-                  <button>Atualizar</button>
-                </li>
-                <li className="disabled">
-                  <button>Informar erro</button>
-                </li>
-              </ul>
-            </details>
+            <h2 className="card-title">{cryptoInfo?.status.name}</h2>
           </div>
-          {!isNull(cryptoInfo) && (
-            <div className="flex flex-col min-[768px]:flex-row gap-x-2">
-              <span className="text-sm  font-semibold">
-                <span className="text-xs font-normal">Quantidade:</span>{' '}
-                {props.amount}
-              </span>
-              <span className="text-sm  font-semibold">
-                {/* There is something pretty wrong here, the avg price isnt updating when adding a new stock */}
-                <span className="text-xs font-normal">Preço médio:</span> R${' '}
-                {props.price.toFixed(2)}
-              </span>
-              {/* <span className="text-sm  font-semibold">
-                <span className="text-xs font-normal">Resultado:</span> %{' '}
-                {getProfit(props.price, cryptoInfo!.regularMarketPrice)}
-              </span>
-              <span className="text-sm  font-semibold">
-                <span className="text-xs font-normal">Carteira:</span> %{' '}
-                {getStockAllocation(
-                  props.amount,
-                  cryptoInfo!.regularMarketPrice,
-                  props.currentBalance,
-                )}
-              </span>
-              <span className="text-sm  font-semibold">
-                <span className="text-xs font-normal">Balanço:</span> R${' '}
-                {getBalance(cryptoInfo!.regularMarketPrice, props.amount)}
-              </span> */}
-            </div>
-          )}
-          {!isNull(chartData) && (
-            <>
-              <h1 className="font-semibold">
-                Variação de preço desde a compra
-              </h1>
-              <InvestementCardChart
-                dates={chartData.dates}
-                prices={chartData.prices}
-                range={chartData.range}
-              />
-            </>
-          )}
 
-          <div className="card-actions">
-            <button disabled className="btn btn-primary w-full">
-              Mais detalhes
-            </button>
+          <details className="dropdown dropdown-end">
+            <summary className="m-1 btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 20 20">
+                <circle cx="10" cy="4" r="2" fill="currentColor" />
+                <circle cx="10" cy="10" r="2" fill="currentColor" />
+                <circle cx="10" cy="16" r="2" fill="currentColor" />
+              </svg>
+            </summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 glassy-border">
+              <li>
+                <button onClick={deleteSelectedCrypto}>Deletar</button>
+              </li>
+              <li className="disabled">
+                <button>Atualizar</button>
+              </li>
+              <li className="disabled">
+                <button>Informar erro</button>
+              </li>
+            </ul>
+          </details>
+        </div>
+        {!isNull(cryptoInfo) && (
+          <div className="flex flex-col min-[768px]:flex-row gap-x-2">
+            <span className="text-sm  font-semibold">
+              <span className="text-xs font-normal">Quantidade:</span>{' '}
+              {props.amount}
+            </span>
+            <span className="text-sm  font-semibold">
+              <span className="text-xs font-normal">Preço médio:</span> R${' '}
+              {props.price.toFixed(2)}
+            </span>
+            <span className="text-sm  font-semibold">
+              <span className="text-xs font-normal">Resultado:</span> %{' '}
+              {getProfit(props.price, cryptoInfo.status.price)}
+            </span>
+            <span className="text-sm  font-semibold">
+              <span className="text-xs font-normal">Carteira:</span> %{' '}
+              {getStockAllocation(
+                props.amount,
+                cryptoInfo.status.price,
+                props.currentBalance,
+              )}
+            </span>
+            <span className="text-sm  font-semibold">
+              <span className="text-xs font-normal">Balanço:</span> R${' '}
+              {getBalance(cryptoInfo.status.price, props.amount)}
+            </span>
           </div>
+        )}
+        {!isNull(chartData) && (
+          <>
+            <h1 className="font-semibold">Variação de preço desde a compra</h1>
+            <InvestementCardChart
+              dates={chartData.dates}
+              prices={chartData.prices}
+              range={chartData.range}
+            />
+          </>
+        )}
+
+        <div className="card-actions">
+          <button disabled className="btn btn-primary w-full">
+            Mais detalhes
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
