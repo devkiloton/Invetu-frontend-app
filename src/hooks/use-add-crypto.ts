c968df83f00816a46cfc1c3c8b02e99cd1507c5e
devkiloton <devkiloton@gmail.com>
2023-12-29 16:23:24 -0300

    Creating hook to add crypto

--- src/hooks/use-add-crypto.ts
+++ src/hooks/use-add-crypto.ts
@@ -1,20 +1,32 @@
-import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { firebaseClient } from '~/clients/firebase-client/firebase-client';
 import { Crypto } from '~/clients/firebase-client/models/Investments';
+import { addCryptoData } from '~/features/investments-data/investments-data-slice';
+import { addCrypto } from '~/features/investments/investments-slice';
 import { useAuth } from '~/lib/firebase';
+import useTooltip from './use-tooltip';
 
 function useAddCrypto() {
   const dispatch = useDispatch();
   const auth = useAuth();
-  return useCallback(
-    (investment: Crypto) => {
-      firebaseClient().firestore.investments.cryptoCurrencies.add({
+  const tooltip = useTooltip();
+
+  return (investment: Crypto) => {
+    firebaseClient()
+      .firestore.investments.cryptoCurrencies.add({
         ...investment,
+        ticker: investment.ticker.toLowerCase(),
         userID: auth.currentUser?.uid!,
+      })
+      .then(() => {
+        tooltip('Criptomoeda adicionada com sucesso!');
+        firebaseClient()
+          .functions.findCryptosData([investment.ticker.toLowerCase()], 'all')
+          .then(res => {
+            dispatch(addCrypto(investment));
+            dispatch(addCryptoData(res[0]));
+          });
       });
-    },
-    [dispatch],
-  );
+  };
 }
 export default useAddCrypto;
