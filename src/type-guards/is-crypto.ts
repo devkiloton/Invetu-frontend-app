7112afd520d2f485151f8112c6f1f7494d6205ec
devkiloton <devkiloton@gmail.com>
2023-12-31 01:43:29 -0300

    Updating isCrypto type guard

--- src/type-guards/is-crypto.ts
+++ src/type-guards/is-crypto.ts
@@ -1,6 +1,5 @@
-import { isNil } from 'lodash-es';
 import { Crypto } from '~/clients/firebase-client/models/Investments';
 
 export const isCrypto = (item: any): item is Crypto => {
-  return isNil(item?.type);
+  return item?.name;
 };
