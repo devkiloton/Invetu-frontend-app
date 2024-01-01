857a97346bd6c894228ae2e67488eae51d686c79
devkiloton <devkiloton@gmail.com>
2024-01-01 18:27:33 -0300

    Fn to get previous month date

---
+++ src/helpers/get-previous-month-date.ts
@@ -0,0 +1,10 @@
+/**
+ * Expect to receive a date in string format DD/MM/YYYY and return the string in format DD/MM/YYYY of the previous month
+ * @param date - A date in string format DD/MM/YYYY
+ */
+export const getPreviousMonthDate = (date: string): string => {
+  const [day, month, year] = date.split('/');
+  const dateObj = new Date(`${year}-${month}-${day}`);
+  dateObj.setMonth(dateObj.getMonth() - 1);
+  return dateObj.toLocaleDateString('pt-BR').split(',')[0];
+};
