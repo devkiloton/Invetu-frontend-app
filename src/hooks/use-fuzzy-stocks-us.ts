c19617a2ce0ddbca15827c22b819a503fcb471b1
devkiloton <devkiloton@gmail.com>
2024-01-03 21:18:12 -0300

    Adding docs

--- src/hooks/use-fuzzy-stocks-us.ts
+++ src/hooks/use-fuzzy-stocks-us.ts
@@ -1,7 +1,10 @@
 import { useCallback } from 'react';
 import { firebaseClient } from '~/clients/firebase-client/firebase-client';
 import { FuzzyUS } from '~/clients/firebase-client/models/fuzzy-us';
-
+/**
+ * Hook responsible for making fuzzy search in the backend and set the results using callback function
+ * @returns void
+ */
 export default function useFuzzyStocksUS() {
   return useCallback(
     (ticker: string, callback: (symbols: string[]) => void) => {
