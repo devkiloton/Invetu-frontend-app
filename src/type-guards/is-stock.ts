8d77125530c8c61a39f0a1d970d9b562017f5837
devkiloton <devkiloton@gmail.com>
2024-01-06 14:45:26 -0300

    Improving typeguard

--- src/type-guards/is-stock.ts
+++ src/type-guards/is-stock.ts
@@ -1,5 +1,6 @@
+import { isNil } from 'lodash-es';
 import { Stock } from '~/clients/firebase-client/models/Investments';
 
 export const isStock = (item: any): item is Stock => {
-  return 'type' in item;
+  return !isNil(item?.type);
 };
