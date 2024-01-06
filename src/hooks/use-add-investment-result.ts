3043681943bcacff36d081fe162d8b52561b00b5
devkiloton <devkiloton@gmail.com>
2024-01-06 15:05:18 -0300

    Creating hook to add investment result

---
+++ src/hooks/use-add-investment-result.ts
@@ -0,0 +1,11 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { InvestmentResult, InvestmentType, addInvestmentResult } from '~/features/investments-result-slice/investments-result-slice';
+
+function useAddInvestmentResult() {
+  const dispatch = useDispatch();
+  return useCallback((investmentResult: InvestmentResult, investmentType: InvestmentType) => { 
+    dispatch(addInvestmentResult({...investmentResult, type: investmentType}));
+  }, []);
+}
+export default useAddInvestmentResult;
