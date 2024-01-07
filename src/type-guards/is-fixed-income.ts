28ae4884d12a3c65e21f67f3856cea314d4d65d1
devkiloton <devkiloton@gmail.com>
2024-01-07 16:25:13 -0300

    Updating type guard

--- src/type-guards/is-fixed-income.ts
+++ src/type-guards/is-fixed-income.ts
@@ -2,5 +2,5 @@ import { isNil } from 'lodash-es';
 import { FixedIncome } from '~/clients/firebase-client/models/Investments';
 
 export const isFixedIncome = (item: any): item is FixedIncome => {
-  return isNil(item?.index);
+  return !isNil(item?.index);
 };
