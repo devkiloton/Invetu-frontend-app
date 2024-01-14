bd0dbeb48b568fd94e634dd221bfeede02d9a30d
devkiloton <dev.kiloton@gmail.com>
2024-01-14 15:22:05 -0300

    Changing version

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar = () => {
             <div className="flex flex-col">
               <span className="leading-4">Invetu</span>
               <span className="text-[0.6rem] leading-3 font-normal text-start">
-                0.0.1-alpha
+                0.1.0-alpha
               </span>
             </div>
           </a>
