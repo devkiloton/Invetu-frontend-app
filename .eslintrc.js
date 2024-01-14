ab34b5cce618a1fcbc2ae6c255808e84237df608
devkiloton <dev.kiloton@gmail.com>
2024-01-14 00:36:00 -0300

    Running npm run format

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended','plugin:react/recommended', 'plugin:react-hooks/recommended', 'standard', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
